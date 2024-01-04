fb94603c70935ec2aaee6eab67bd4d1c7551329b
mybearworld <mybearworld@users.noreply.github.com>
2024-01-04 21:08:58 +0000

    deploy: efed1ddc39e6878e91824845814aac361d03cd53

--- assets/browser-tBxJ_ayQ.js
+++ assets/browser-tBxJ_ayQ.js
@@ -1,4 +1,4 @@
-import { g as getDefaultExportFromCjs, __tla as __tla_0 } from "./index-lBzRUhju.js";
+import { g as getDefaultExportFromCjs, __tla as __tla_0 } from "./index-cjxBBm2f.js";
 let browser$2;
 let __tla = Promise.all([
   (() => {
