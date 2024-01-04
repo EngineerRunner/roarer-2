1a95d28bbc19d007f4aa1d8cdd70ce75dd2c86c7
mybearworld <mybearworld@users.noreply.github.com>
2024-01-04 21:00:26 +0000

    deploy: dfc273568a1e9ffe07a4380a16096b2e924bf178

---
+++ assets/browser-VY2Mltx2.js
@@ -0,0 +1,46 @@
+import { g as getDefaultExportFromCjs, __tla as __tla_0 } from "./index-lBzRUhju.js";
+let browser$2;
+let __tla = Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  function _mergeNamespaces(n, m) {
+    for (var i = 0; i < m.length; i++) {
+      const e = m[i];
+      if (typeof e !== "string" && !Array.isArray(e)) {
+        for (const k in e) {
+          if (k !== "default" && !(k in n)) {
+            const d = Object.getOwnPropertyDescriptor(e, k);
+            if (d) {
+              Object.defineProperty(n, k, d.get ? d : {
+                enumerable: true,
+                get: () => e[k]
+              });
+            }
+          }
+        }
+      }
+    }
+    return Object.freeze(Object.defineProperty(n, Symbol.toStringTag, {
+      value: "Module"
+    }));
+  }
+  var browser = function() {
+    throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object");
+  };
+  const browser$1 = getDefaultExportFromCjs(browser);
+  browser$2 = _mergeNamespaces({
+    __proto__: null,
+    default: browser$1
+  }, [
+    browser
+  ]);
+});
+export {
+  __tla,
+  browser$2 as b
+};
