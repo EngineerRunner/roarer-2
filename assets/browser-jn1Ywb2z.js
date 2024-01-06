03d72746574f268e597c5baedce260ed179d10a1
mybearworld <mybearworld@users.noreply.github.com>
2024-01-06 10:20:42 +0000

    deploy: 0183266ccdde33bbeb083b6f31157e922d58f90d

--- assets/browser-jn1Ywb2z.js
+++ assets/browser-jn1Ywb2z.js
@@ -1,4 +1,4 @@
-import { g as getDefaultExportFromCjs, __tla as __tla_0 } from "./index-FSOj3hEZ.js";
+import { g as getDefaultExportFromCjs, __tla as __tla_0 } from "./index-Qzt_PWaH.js";
 let browser$2;
 let __tla = Promise.all([
   (() => {
